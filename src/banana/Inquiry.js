3e08805b77f349fc98e9cd0012baf3af01f66eb0
Babigdk <Babigdk@gmail.com>
2023-09-11 04:06:18 -0700

    banana land

--- src/banana/Inquiry.js
+++ src/banana/Inquiry.js
@@ -1,21 +1,65 @@
-import { useState } from "react";
+// import { useState } from "react";
+//
+// const Inquiry = () => {
+//   const [contactInfo, setContactInfo] = useState({
+//     name: "",
+//     email: "",
+//     remarks: "",
+//   });
+//
+//   const onChange = (e) => {
+//     setContactInfo({ ...contactInfo, [e.target.id]: e.target.value });
+//   };
+//
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+//     console.log(contactInfo);
+//     //send
+//   };
 
-const Inquiry = () => {
-  const [contactInfo, setContactInfo] = useState({
-    name: "",
-    email: "",
-    remarks: "",
-  });
+  import React, { useState } from "react";
 
-  const onChange = (e) => {
-    setContactInfo({ ...contactInfo, [e.target.id]: e.target.value });
-  };
+  const Inquiry = () => {
+    const [contactInfo, setContactInfo] = useState({
+      name: "",
+      email: "",
+      remarks: "",
+    });
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    console.log(contactInfo);
-    //send
-  };
+    const onChange = (e) => {
+      setContactInfo({ ...contactInfo, [e.target.id]: e.target.value });
+    };
+
+    const onSubmit = async (e) => {
+      e.preventDefault();
+      console.log(contactInfo);
+
+      try {
+        // Send the contact information to the server for email sending
+        const response = await fetch("/send-email", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(contactInfo),
+        });
+
+        if (response.ok) {
+          // Reset the form after successful submission
+          setContactInfo({
+            name: "",
+            email: "",
+            remarks: "",
+          });
+          console.log("Email sent successfully.");
+        } else {
+          console.error("Failed to send email.");
+        }
+      } catch (error) {
+        console.error("Error sending email:", error);
+      }
+    };
+
   return (
     <form className="mt-2">
       <div className="form-group">
